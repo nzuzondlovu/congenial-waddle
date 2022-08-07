5bb3fb82811eaac5b34ae7fc268f0c47dd8e21a6
Nzuzo Ndlovu <nzuzondlovu@gmail.com>
2022-08-07 19:07:06 +0200

    Added migrations for country and employees table

---
+++ database/migrations/2022_08_07_170119_create_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('contact_number');
+            $table->string('email_address');
+            $table->string('date_of_birth');
+            $table->string('street_address');
+            $table->string('city');
+            $table->integer('postal_code');
+            $table->bigInteger('country_id')->unsigned()->index();
+            $table->foreign('country_id')->references('id')->on('countrys')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
