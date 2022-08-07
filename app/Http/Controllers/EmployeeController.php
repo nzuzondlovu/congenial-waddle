90a99b4264b56e1ab240236abc2ecc92cdec7f08
Nzuzo Ndlovu <nzuzondlovu@gmail.com>
2022-08-07 19:41:22 +0200

    Added the functions to handle the web routes under employee controller

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        # code...
+    }
+
+    public function store(Request $request)
+    {
+        # code...
+    }
+
+    public function show(Request $request)
+    {
+        # code...
+    }
+
+    public function update(Request $request)
+    {
+        # code...
+    }
+
+    public function edit(Request $request)
+    {
+        # code...
+    }
 }
